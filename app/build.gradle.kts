ee31e4fc413e8c85953714375e04cd4275aab783
Fabiano Pinto <fabianopinto@gmail.com>
2026-02-23 15:17:19 -0300

    Add AndroidX Navigation Compose dependency and configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
 
     // Room
     implementation(libs.room.runtime)
